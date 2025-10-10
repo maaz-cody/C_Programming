8117371ecb0fbf72c5f6e88277ce5ce0c85288d8
maaz-cody <maazsayyed@eng.rizvi.edu.in>
2025-10-10 12:33:33 +0530

    Add files via upload

---
+++ Exp2a.c
@@ -0,0 +1,24 @@
+/*
+Name: Maaz Ismail Sayyed
+UIN: 251M019
+Class: FE Mechanical Engineering
+Div: F(F1_
+*/
+#include <stdio.h>
+int main()
+{
+    int x;
+    printf("Enter your desired number:");
+    scanf("%d",&x);
+    
+    if(x%2==0)
+    {
+        printf("Your given number is even");
+    }
+    
+    else
+    {
+        printf("Your given number is odd");
+    }
+ return 0;
+}
