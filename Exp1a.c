76ce8cecc783c660dafc4d6e3891138d10209f52
maaz-cody <maazsayyed@eng.rizvi.edu.in>
2025-10-12 18:57:42 +0530

    Add files via upload

---
+++ Exp1a.c
@@ -0,0 +1,21 @@
+/*
+Name: Maaz Ismail Sayyed
+UIN: 251M019
+Class: FE Mechanical Engineering
+Div: F(F1_
+*/
+
+
+#include <stdio.h>
+
+int main()
+{
+    printf("\t MY NAME IS MAAZ ISMAIL SAYYED \n");
+printf("\t MY AGE IS 19Yrs NEXT YEAR I WILL BE 20YRS OLD\n");
+printf("\t USUALLY MY CLASS NO. ID 504\n");
+printf("\t I'M FROM MECHANICAL ENGINEERING BRANCH,\n");
+printf("\t MY UIN (UNIQUE IDEENTIFICATION NUMBER) IS 251M019\n");
+printf("\t I'M IN F1 DIVISION");
+
+    return 0;
+}
