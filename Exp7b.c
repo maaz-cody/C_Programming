e372465a90a1b833fc390b478bb3bbbff5da9867
maaz-cody <maazsayyed@eng.rizvi.edu.in>
2025-11-14 10:30:49 +0530

    Add files via upload

---
+++ Exp7b.c
@@ -0,0 +1,53 @@
+/*
+Name: Maaz Ismail Sayyed
+UIN: 251M019
+Class: FE Mechanical Engineering
+Div: F(F1)
+*/
+
+#include <stdio.h>
+
+int main() {
+    int a[10][10], b[10][10], sum[10][10];
+    int rows, cols;
+
+    // Input number of rows and columns
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+    printf("Enter number of columns: ");
+    scanf("%d", &cols);
+
+    // Input matrix A
+    printf("\nEnter elements of Matrix A:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    // Input matrix B
+    printf("\nEnter elements of Matrix B:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            scanf("%d", &b[i][j]);
+        }
+    }
+
+    // Add the matrices
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            sum[i][j] = a[i][j] + b[i][j];
+        }
+    }
+
+    // Display result
+    printf("\nSum of the two matrices:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%d ", sum[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
